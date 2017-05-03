6ac6827ba4406e84a4681bbe7db0a8b432e4d764
zoc6 <newmessiah@gmx.net>
2017-05-03 14:35:38 +0200

    HelloGit
    TestProject for GitHub
    
    Created by c.b. 03.05.2017

---
+++ HelloGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Git");
+      Console.ReadLine();
+    }
+  }
+}
